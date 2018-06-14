ce15e23375fca5f87d0665d103faa3cae3d85af3
Maken Hubbard <makhub26@outlook.com>
2018-06-13 19:12:52 -0500

    adding the logout format and functionality

--- javascripts/auth/authEvents.js
+++ javascripts/auth/authEvents.js
@@ -1,3 +1,15 @@
+const logOut = (e) => {
+  e.preventDefault();
+  firebase.auth().signOut()
+    .then((results) => {
+      if (results === null) {
+        viewSplashPage();
+      }
+    }).catch(function (error) {
+      console.error('error in log out', error);
+    });
+};
+
 const viewRegister = () => {
   $('#register-form').removeClass('hide');
   $('#signin-form').addClass('hide');
@@ -8,9 +20,15 @@ const viewSignin = () => {
   $('#register-form').addClass('hide');
   $('#splash').addClass('hide');
 };
+const viewSplashPage = () => {
+  $('signin-form').addClass('hide');
+  $('register-form').addClass('hide');
+  $('splash').removeClass('hide');
+};
 const authEvents = () => {
   $('#jumbo-signin, #signin-link').click(viewSignin);
   $('#jumbo-register, #register-link').click(viewRegister);
+  $('#nav-bar-area, #logOutButt').click(logOut);
 };
 module.exports = {
   authEvents,
