32d2901951020331b225b600b9e35f96a8a1a872
Brian Wright <briankw23@gmail.com>
2018-06-13 19:26:13 -0500

    login w/ fire works and switches to dashboard view

--- javascripts/auth/authEvents.js
+++ javascripts/auth/authEvents.js
@@ -8,10 +8,33 @@ const viewSignin = () => {
   $('#register-form').addClass('hide');
   $('#splash').addClass('hide');
 };
+
+const logInNutShell = () => {
+  $('#signin-btn').click((e) => {
+    e.preventDefault();
+    const email = $('#signin-email').val();
+    const pass = $('#signin-pass').val();
+    firebase.auth().signInWithEmailAndPassword(email, pass)
+      .then((user) => {
+        dashBoardView();
+      })
+      .catch((error) => {
+        console.error(error.message);
+      });
+  });
+};
+
+const dashBoardView = () => {
+  $('#main-view').removeClass('hide');
+  $('#authentication').addClass('hide');
+};
+
 const authEvents = () => {
   $('#jumbo-signin, #signin-link').click(viewSignin);
   $('#jumbo-register, #register-link').click(viewRegister);
+  logInNutShell();
 };
+
 module.exports = {
   authEvents,
 };
