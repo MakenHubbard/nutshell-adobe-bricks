e31874deca9d3ae6da0e83894ab5421a005fa36d
Brian Wright <briankw23@gmail.com>
2018-06-15 19:24:10 -0500

    wip favicon showing

--- javascripts/newsArtitcles/newsDom.js
+++ javascripts/newsArtitcles/newsDom.js
@@ -1,18 +1,26 @@
 const buildUpArticle = (articlesArray) => {
   console.log(articlesArray);
-
   let string = '';
-  string += `<div class="media">`;
-  string += `<div class="media-left media-middle">`;
-  string +=  `<a href="#">`;
-  string +=    `<img class="media-object" src="..." alt="...">`;
-  string +=  `</a>`;
-  string += `</div>`;
-  string += `<div class="media-body">`;
-  string +=  `<h4 class="media-heading">Middle aligned media</h4>`;
-  string +=  `</div>`;
-  string +=  `</div>`;
-  return string;
+  for (let i = 0; i < articlesArray.length; i++) {
+    string += `<div class="media">`;
+    string += `<div class="media-left media-middle">`;
+    string +=  `<a href="${articlesArray[i].url}">`;
+    const url = articlesArray[i].url;
+    const rootUrl = url.match('^.+?[^\/:](?=[?\/]|$)');
+    string +=    `<img class="media-object" src="${rootUrl}/favicon.ico" alt="...">`;
+    string +=  `</a>`;
+    string += `</div>`;
+    string += `<div class="media-body">`;
+    string +=  `<h4 class="media-heading">${articlesArray[i].title}</h4>`;
+    string += `<p>${articlesArray[i].synopsis}</p>`;
+    string +=  `</div>`;
+    string +=  `</div>`;
+  }
+  printArticles(string);
+};
+
+const printArticles = (strang) => {
+  $('#newsFeed').html(strang);
 };
 
 module.exports = buildUpArticle;
